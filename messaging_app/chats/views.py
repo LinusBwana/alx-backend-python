31407675c8c70122e21aca1da978b37c64d17f73
LinusBwana <linusbwana49@gmail.com>
2025-09-19 00:43:42 +0300

    Implemented CRUD endpoints for conversations and messages
    - Added ConversationViewSet with list, create, retrieve, update, and delete actions
    - Added MessageViewSet with list, create, retrieve, update, and delete actions
    - Integrated get_object_or_404 for safe object retrieval
    - Added support for bulk creation using `many=True` in serializers
    - Included appropriate HTTP status codes for each action

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,105 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status, filters
+from .serializers import ConversationSerializer, MessageSerializer
+from .models import Conversation, Message
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
+class ConversationViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Conversation.objects.all()
+
+        search_query = request.query_params.get('search')
+        if search_query:
+            queryset = queryset.filter(
+                participants_id__first_name__icontains=search_query
+            ) | queryset.filter(
+                participants_id__last_name__icontains=search_query
+            ) | queryset.filter(
+                participants_id__email__icontains=search_query
+            )
+
+        # --- Manual ordering ---
+        ordering = request.query_params.get('ordering')
+        if ordering:
+            queryset = queryset.order_by(ordering)
+
+        serializer = ConversationSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        many = isinstance(request.data, list)
+        serializer = ConversationSerializer(data=request.data, many=many)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def retrieve(self, request, pk=None):
+        conversation = get_object_or_404(Conversation, pk=pk)
+        serializer = Message.objects(conversation)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def update(self, request, pk=None):
+        conversation = get_object_or_404(Conversation, pk=pk)
+        serializer = MessageSerializer(conversation, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def Delete(self, request, pk=None):
+        conversation = get_object_or_404(Conversation, pk=pk)
+        conversation.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class MessageViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset  = Message.objects.all()
+
+        search_query = request.query_params.get('search')
+        if search_query:
+            queryset = queryset.filter(
+                message_body__icontains=search_query
+            ) | queryset.filter(
+                sender_id__first_name__icontains=search_query
+            ) | queryset.filter(
+                sender_id__last_name__icontains=search_query
+            ) | queryset.filter(
+                sender_id__email__icontains=search_query
+            )
+
+        # --- Manual ordering ---
+        ordering = request.query_params.get('ordering')
+        if ordering:
+            queryset = queryset.order_by(ordering)
+
+        serializer = MessageSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        many = isinstance(request.data, list)
+        serializer = MessageSerializer(data=request.data, many=many)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def retrieve(self, request, pk=None):
+        message = get_object_or_404(Message, pk=pk)
+        serializer = Message.objects(message)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def update(self, request, pk=None):
+        message = get_object_or_404(Message, pk=pk)
+        serializer = MessageSerializer(message, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def Delete(self, request, pk=None):
+        message = get_object_or_404(Message, pk=pk)
+        message.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from chats import views
+
+router = DefaultRouter()
+router.register(views.MessageViewSet, basename='messages')
+router.register(views.ConversationViewSet, basename='conversations')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('router.urls')),
 ]
