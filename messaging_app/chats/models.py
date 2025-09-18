62ea89b1bfe0b7aa277edb6cec0b46bfee4ee69c
LinusBwana <linusbwana49@gmail.com>
2025-09-18 16:12:00 +0300

    Added models for users, messages, and conversations in users.models
    - Created `User` model with roles (guest, host, admin) and authentication fields
    - Added `Message` model to store chat messages with sender and receiver relationships
    - Implemented `Conversation` model to manage group/user chat threads
    - Added timestamps and indexing for performance
    - Prepared models for future integration with Django REST Framework

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,81 @@
+import uuid
 from django.db import models
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    Role_Choices = [
+        ('guest', 'Guest'),
+        ('host', 'Host'),
+        ('admin', 'Admin'),
+    ]  
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        db_index=True
+    )
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(unique=True, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    # password_hash = models.CharField(max_length=255, null=False, blank=False)
+    role = models.CharField(
+        max_length=10,
+        choices=Role_Choices,
+        default='guest',
+        null=False,
+        blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name', 'phone_number']
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['email']),
+            models.Index(fields=['role']),
+        ]
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
+    
+
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        db_index=True
+    )
+    participants_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.conversation_id}"
+
+
+class Message(models.Model):
+    message_id = models.URLField(
+        primary_key=True,
+        default = uuid.uuid4,
+        editable=False,
+        db_index=True
+    )
+    sender_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    message_body = models.TextField(null=False)
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['sent_at']
+        indexes = [
+            models.Index(fields=['message_body', 'sent_at']),
+        ]
+
+    def __str__(self):
+            preview = self.message_body[:50] + "..." if len(self.message_body) > 50 else self.message_body
+            return f"{self.sender_id}: {preview}"
